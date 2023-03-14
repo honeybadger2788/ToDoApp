1e908564b2b6eb238eab22df50bf78615f698b47
honeybadger2788 <noe.carosella@gmail.com>
2023-03-14 10:03:57 -0300

    add live data in viewmodel

--- app/src/main/java/com/example/todoapp/addTasks/ui/TasksScreen.kt
+++ app/src/main/java/com/example/todoapp/addTasks/ui/TasksScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -14,23 +15,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 
-@Preview
+
 @Composable
-fun TasksScreen() {
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
+
     Box(Modifier.fillMaxSize()) {
-        AddTasksDialog(show = true, onDismiss = {}, onTaskAdded = {})
-        FabDialog(Modifier.align(Alignment.BottomEnd))
+        AddTasksDialog(
+            show = showDialog,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTaskCreated(it) })
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
     }
 }
 
 @Composable
-fun FabDialog(modifier: Modifier) {
-    FloatingActionButton(onClick = { /*TODO*/ },
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
+    FloatingActionButton(onClick = { tasksViewModel.onShowDialogClick() },
         modifier = modifier.padding(16.dp)
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "")
@@ -45,7 +50,11 @@ fun AddTasksDialog( show: Boolean, onDismiss:() -> Unit, onTaskAdded:(String) ->
 
     if(show){
         Dialog(onDismissRequest = { onDismiss() }) {
-            Column(Modifier.fillMaxWidth().background(Color.White).padding(16.dp)) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)) {
                 Text(
                     text = "Agregar tarea",
                     fontSize = 18.sp,
--- app/src/main/java/com/example/todoapp/addTasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/addTasks/ui/TasksViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.todoapp.addTasks.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class TasksViewModel @Inject constructor():ViewModel() {
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated(task: String) {
+        _showDialog.value = false
+        Log.i("Noe", "Ok")
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
 }
